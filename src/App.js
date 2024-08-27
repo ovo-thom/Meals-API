096e8712665e350ffa88a38ce85016eed97ab6eb
Thomas OvO <thonnad.thomas@gmail.com>
2024-08-27 16:36:06 +0200

    meals api react fromscratch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './index.css';
+import axios from "axios";
+import Card from './components/Card';
 
 function App() {
+
+  const [mealsData, setMealsData] = useState([]);
+  const [inputSearch, setInputSearch] = useState("");
+
+useEffect(() => {
+axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + inputSearch).then((res) => setMealsData(res.data.meals));
+}, [inputSearch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>React Cooking App</h1>
+      <input type="text" placeholder="Entre le nom d'un aliment (en anglais)" onChange={(e) => setInputSearch(e.target.value)}/>
+      <div className="meals-container">
+        {mealsData && mealsData
+        .slice(0, 24)
+        .map((meal) =>
+        <Card key={meal.idMeal} meal={meal} /> )}
+      </div>
     </div>
   );
 }
